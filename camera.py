5d8d75a7951702189ee365671e87b511379ca6fe
Mario López <mlopez@affinipay.com>
2021-10-08 16:24:27 -0500

    Modify pipeline for mask'

--- camera.py
+++ camera.py
@@ -1,14 +1,14 @@
 import time
-from queue import Queue, Empty
+from queue import Queue, Empty, Full
 from threading import Thread
 
 import cv2
-import mediapipe as mp
 import numpy as np
 from pyfakewebcam import FakeWebcam
 
 from camera.producers.background import read_background
 from camera.producers.camera import Camera
+from pipeline.transformers import *
 
 
 # Todo: make sure that the fps match between the writer and video source (webcam or video background)
@@ -40,22 +40,17 @@ class FrameCounter:
 
 def cam_reader(camera, frames, frame_counter):
     while True:
-        frames.put(camera.next())
-
-
-def cam_writer(fake_cam, frames, frame_counter, target_fps):
-    # target fps for the frames
-    frame_timeout = 1 / target_fps
-    # get the very first frame as we will use it as the baseline in case we start slow
-    frame = frames.get()
-    while True:
-        fake_cam.schedule_frame(frame)
-        frame_counter.meter()
         try:
-            frame = frames.get(timeout=frame_timeout)
-        except Empty:
-            # We will just put the last frame instead of waiting for a new one from the queue
+            frames.put_nowait(camera.next())
+        except Full:
+            print(f"Dropping camera frames as queue is full")
             pass
+
+
+def cam_writer(fake_cam, frames, frame_counter):
+    while True:
+        fake_cam.schedule_frame(frames.get())
+        frame_counter.meter()
 
 
 def frame_printer(frame_counter, writer, reader):
@@ -64,27 +59,40 @@ def frame_printer(frame_counter, writer, reader):
         print("FPS: {:6.2f} writer({}) reader({})".format(frame_counter.fps(), writer.qsize(), reader.qsize()))
 
 
-old_mask = None
+def extract_background_v2(algo, frame, old_mask, threshold, mask_update_speed):
+    BG_COLOR = (192, 192, 192)  # gray
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = algo.process(image)
 
-BG_COLOR = (192, 192, 192)  # gray
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-
-def extract_background(algo, frame, old_mask, threshold, mask_update_speed):
-    mask = algo.process(frame).segmentation_mask
-    cv2.threshold(mask, threshold, 1, cv2.THRESH_BINARY, dst=mask)
-    cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1, dst=mask)
-    cv2.blur(mask, (10, 10), dst=mask)
-    if old_mask is None:
-        old_mask = mask
-    mask = cv2.accumulateWeighted(mask, old_mask, mask_update_speed)
-    return mask
+    # Draw selfie segmentation on the background image.
+    # To improve segmentation around boundaries, consider applying a joint
+    # bilateral filter to "results.segmentation_mask" with "image".
+    condition = np.stack(
+        (results.segmentation_mask,) * 3, axis=-1) > 0.1
+    # The background can be customized.
+    #   a) Load an image (with the same width and height of the input image) to
+    #      be the background, e.g., bg_image = cv2.imread('/path/to/image/file')
+    #   b) Blur the input image by applying image filtering, e.g.,
+    #      bg_image = cv2.GaussianBlur(image,(55,55),0)
+    bg_image = np.zeros(image.shape, dtype=np.uint8)
+    bg_image[:] = BG_COLOR
+    output_image = np.where(condition, image, bg_image)
+    return output_image
 
 
 # todo: make sure to match fps between video file and camera as it could be inconsistent after initialization
 
 def main():
     video_background = '/home/mlopez/Downloads/Seemed - 3639.mp4'
-    image_background = '/home/mlopez/Pictures/Backgrounds/fursona.jpg'
+    image_background = '/home/mlopez/Desktop/background.jpg'
     target_fps = 30
     width = 640
     height = 360
@@ -92,26 +100,57 @@ def main():
     print(f'Target fps {target_fps}')
     camera = Camera('/dev/video0', width, height, target_fps, 'YUYV')
     fake_cam = FakeWebcam('/dev/video2', width, height)
-    source_pipe = Queue()
-    destination_pipe = Queue()
+    # Queue should honestly drop oldest elements if full
+    source_pipe = Queue(maxsize=5)
+    destination_pipe = Queue(maxsize=5)
     frame_counter = FrameCounter()
     frame_counter.start()
     writer = Thread(target=cam_reader, args=(camera, source_pipe, frame_counter))
-    reader = Thread(target=cam_writer, args=(fake_cam, destination_pipe, frame_counter, target_fps))
+    reader = Thread(target=cam_writer, args=(fake_cam, destination_pipe, frame_counter))
     printer = Thread(target=frame_printer, args=(frame_counter, destination_pipe, source_pipe))
     reader.start()
     writer.start()
     printer.start()
     old_mask = None
-    # Selfie segmentation could cause a memory leak if we create it multiple times
-    algo = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection=1)
+    frame = source_pipe.get(block=True)
+    frame_timeout = 1 / target_fps
+    print(f'{target_fps} {frame_timeout}')
+    mask_pipeline = build_mask_pipeline(0.75, 1, 0.5)
+
+    # The background provider is a lambda of the current process
+    provider = lambda: background.next(frame_counter.fps())
+    linear_background_apply = LinearBlendBackgroundReplace(provider)
+    background_apply = BackgroundReplace(provider)
     while True:
-        frame = source_pipe.get(block=True)
-        mask = extract_background(algo, frame, old_mask, 0.5, 0.5)
-        old_mask = mask
-        cv2.blendLinear(frame, background.next(frame_counter.fps()), mask, 1 - mask, dst=frame)
-        transformation = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        destination_pipe.put(transformation)
+        try:
+            frame = source_pipe.get(timeout=frame_timeout)
+        except Empty:
+            # Use the already existing frame to write it
+            pass
+        """
+        image = extract_background_v2(algo, frame, old_mask, 0.5, 1)
+        destination_pipe.put(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        """
+        mask = mask_pipeline.process(frame)
+        result = cv2.cvtColor(background_apply.process(frame, mask), cv2.COLOR_BGR2RGB)
+        try:
+            destination_pipe.put(result)
+        except Full:
+            print('Dropped write frame')
+            pass
+
+
+def build_mask_pipeline(threshold, dilate_iterations, mask_update_speed):
+    # Create the mask pipeline, in this case the pipeline would be build at the beginning and then we would apply
+    # Everything at runtime
+    return Pipeline() \
+        .attach(SelfieSegmentation()) \
+        .attach(BilateralFilter()) \
+        .attach(Threshold(threshold=0.05)) \
+        .attach(Dilate(iterations=1)) \
+        .attach(Sigmoid()) \
+        .attach(AccumulatedWeighted(0.5)) \
+        .build()
 
 
 if __name__ == "__main__":
---
+++ pipeline/transformers.py
@@ -0,0 +1,142 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+class Pipeline:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+
+    def attach(self, transformer):
+        if self._head is None:
+            self._head = transformer
+            self._tail = transformer
+        else:
+            self._tail.attach(transformer)
+            self._tail = transformer
+        return self
+
+    def build(self):
+        return self._head
+
+
+class Transformer:
+
+    def __init__(self, overwrite_source=True):
+        self._next = None
+        self._overwrite_source = overwrite_source
+
+    def attach(self, transformer):
+        self._next = transformer
+
+    def apply(self, source):
+        raise RuntimeError("Apply not implemented")
+
+    def process(self, frame):
+        current = frame if self._overwrite_source else frame.copy()
+        transformation = self.apply(current)
+        if self._next:
+            return self._next.process(transformation)
+        return transformation
+
+
+class SelfieSegmentation(Transformer):
+    def __init__(self):
+        super().__init__()
+        self._algo = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection=1)
+
+    def apply(self, source):
+        source.flags.writeable = False
+        result = self._algo.process(cv2.cvtColor(source, cv2.COLOR_BGR2RGB)).segmentation_mask
+        source.flags.writeable = True
+        return result
+
+
+class Threshold(Transformer):
+
+    def __init__(self, threshold=1):
+        super().__init__()
+        self._threshold = threshold
+        self._type = cv2.THRESH_BINARY
+        self._max_value = 1
+
+    def apply(self, source):
+        cv2.threshold(source, self._threshold, self._max_value, self._type, dst=source)
+        return source
+
+
+class Dilate(Transformer):
+    def __init__(self, iterations=1):
+        super().__init__()
+        self._iterations = iterations
+        self._kernel = np.ones((5, 5), np.uint8)
+
+    def apply(self, source):
+        cv2.dilate(source, self._kernel, iterations=self._iterations, dst=source)
+        return source
+
+
+class Blur(Transformer):
+    def __init__(self):
+        super().__init__()
+        self._kernel = (10, 10)
+
+    def apply(self, source):
+        cv2.blur(source, self._kernel, source)
+        return source
+
+
+class BilateralFilter(Transformer):
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, source):
+        return cv2.bilateralFilter(source, 5, 200, 200)
+
+
+class AccumulatedWeighted(Transformer):
+    def __init__(self, update_speed):
+        super().__init__()
+        self._update_speed = update_speed
+        self._last_source = None
+
+    def apply(self, source):
+        if self._last_source is None:
+            self._last_source = source
+        return cv2.accumulateWeighted(source, self._last_source, self._update_speed)
+
+
+class Sigmoid(Transformer):
+    def __init__(self, a=5., b=-10.):
+        super().__init__()
+        self._a = a
+        self._b = b
+
+    def apply(self, source):
+        z = np.exp(self._a + self._b * source)
+        sig = 1 / (1 + z)
+        return sig
+
+
+class LinearBlendBackgroundReplace:
+    def __init__(self, background_provider, overwrite_source=True):
+        super().__init__()
+        self._provider = background_provider
+        self._overwrite_source = overwrite_source
+
+    def process(self, frame, mask):
+        current = frame if self._overwrite_source else frame.copy()
+        cv2.blendLinear(current, self._provider(), mask, 1 - mask, current)
+        return current
+
+
+class BackgroundReplace:
+    def __init__(self, background_provider, threshold=0.1):
+        super().__init__()
+        self._provider = background_provider
+        self._threshold = threshold
+
+    def process(self, frame, mask):
+        condition = np.stack((mask,) * 3, axis=-1) > self._threshold
+        return np.where(condition, frame, self._provider())
